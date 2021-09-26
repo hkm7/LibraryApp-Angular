b7014596937a0e757308180cca8ce9fe95eb84cb
Hrishikesh Mohan <50671806+hkm7@users.noreply.github.com>
2021-09-26 15:43:03 +0530

    updated

--- Lib-App/backend/README.md
+++
@@ -1,6 +0,0 @@
-# LibraryApp
-A webapp that lists books and authors.
-Built using Node JS backend, Express framework and MongoDB database.
-Uses ejs files to render pages.
-Hosted at:
-      https://anotherlibraryapp.herokuapp.com/
--- Lib-App/frontend/src/app/app.component.ts
+++ Lib-App/frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'ZebraLib';
+  constructor(public _authService:AuthService,private _router:Router){}
+
 }
